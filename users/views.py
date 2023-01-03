0ae9f9ba26606f68614dbef1f64696e7960971fd
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-03 20:23:33 +0300

    updated

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model, login, logout
+from .forms import UseRegistrationForm
 
-# Create your views here.
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('/')
+    if request.method=='POST':
+        form = UseRegistrationForm(request.POST)
+        if form.is_valid():
+            user= form.save()
+            login(request,user)
+            return redirect('/')
+        else:
+            for error in list(form.errors.values()):
+                 print(request, error)
+    else:
+         form = UseRegistrationForm()  
+    return render(
+        request = request,
+        template_name = "register.html",
+        context={"form":form}
+    )              
