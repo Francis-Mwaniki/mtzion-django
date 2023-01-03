0d644e734e964650ba5aa5624f9fa9abe7df411a
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-03 22:01:24 +0300

    updated

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 
 class UseRegistrationForm(UserCreationForm):
     email = forms.EmailField(help_text='A valid Email Please!',required=True)
@@ -16,4 +17,13 @@ class UseRegistrationForm(UserCreationForm):
         if commit:
             user.save()
         return user    
-            
+class UserLoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'class': 'form-control', 'placeholder': 'Username or Email'}),
+        label="Username or Email*")
+
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'class': 'form-control', 'placeholder': 'Password'}))           
