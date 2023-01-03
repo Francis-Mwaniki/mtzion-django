9c3e3451cfe0211bf4717400d65ed956f8352f35
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-03 21:51:46 +0300

    updated

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('register/',views.register, name='register')
+    path('register/',views.register, name='register'),
+    path('login', views.custom_login, name='login'),
+    path('logout', views.custom_logout, name='logout'),
 ]
