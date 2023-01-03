e6a78a37212296c8f9f108430d6582d340b6728f
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-03 21:52:00 +0300

    updated

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('register/',views.register, name='register'),
-    path('login', views.custom_login, name='login'),
-    path('logout', views.custom_logout, name='logout'),
+    path('login/', views.custom_login, name='login'),
+    path('logout/', views.custom_logout, name='logout'),
 ]
