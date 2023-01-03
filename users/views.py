31e30a93e42932c16af176fda30429faa4091f82
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-03 21:42:23 +0300

    updated

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import get_user_model, login, logout
+from django.contrib.auth import get_user_model, login, logout, authenticate
 from .forms import UseRegistrationForm
 from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.user.is_authenticated:
@@ -23,3 +25,35 @@ def register(request):
         template_name = "users/register.html",
         context={"form":form}
     )              
+@login_required
+def custom_logout(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("homepage")    
+def custom_login(request):
+    if request.user.is_authenticated:
+        return redirect('homepage')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+            )
+            if user is not None:
+                login(request, user)
+                messages.success(request, f"Hello <b>{user.username}</b>! You have been logged in")
+                return redirect('homepage')
+
+        else:
+            for error in list(form.errors.values()):
+                messages.error(request, error) 
+
+    form = AuthenticationForm() 
+    
+    return render(
+        request=request,
+        template_name="users/login.html", 
+        context={'form': form}
+        )
