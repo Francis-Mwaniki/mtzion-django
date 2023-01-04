a30a882b5dc806548c5e1fa5398affb827b60114
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-04 12:03:42 +0300

    updated

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+import os
 # Create your models here.
 class CustomUser(AbstractUser):
     
@@ -9,7 +9,10 @@ class CustomUser(AbstractUser):
         ("subscriber", "subscriber"),
         ("moderator","moderator")
     )
-    
+    def image_upload_to(self, instance=None):
+        if instance:
+            return os.path.join("Users", self.username, instance)
+        return None
     email = models.EmailField(unique=True)
     status = models.CharField(max_length=100,choices=STATUS, default='regular')
     description = models.CharField(max_length=600,blank=True,default="")
