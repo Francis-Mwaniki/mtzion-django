4e041900eb0fb983bf0304e54aea9106bd1c0835
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-04 11:21:27 +0300

    updated

--- home/models.py
+++ home/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils import timezone
 from tinymce.models import HTMLField
 from django.contrib.auth import get_user_model
+from django.template.defaultfilters import slugify
+import os
 # Create your models here.
 class TopicSeries(models.Model):
     title = models.CharField(max_length=255)
