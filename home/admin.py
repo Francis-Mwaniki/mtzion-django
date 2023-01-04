b31e33efd848cf7debed481d2b954736645f6f12
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-04 11:30:25 +0300

    updated

--- home/admin.py
+++ home/admin.py
@@ -8,11 +8,12 @@ class TopicSeriesAdmin(admin.ModelAdmin):
         "subtitle",
         "slug",
         'author',
+        'image',
         "published"
     ]
 class TopicsAdmin(admin.ModelAdmin):
        fieldsets = [
-        ("Header", {'fields': ["title", "subtitle", "topic_slug", "series",'author']}),
+        ("Header", {'fields': ["title", "subtitle", "topic_slug", "series",'author','image']}),
         ("Content", {"fields": ["content", "speaker"]}),
         ("Date", {"fields": ["modified"]})
     ]
