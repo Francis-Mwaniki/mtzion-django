c37947a10c27af4d537faed586f3a08b88a4d1b9
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-04 15:12:43 +0300

    updated

--- users/urls.py
+++ users/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('logout/', views.custom_logout, name='logout'),
     path('profile/<username>/',views.profile,name='profile'),
         path('activate/<uidb64>/<token>', views.activate, name='activate'),
-         path("password_change", views.password_change, name="password_change"),
-    path("password_reset", views.password_reset_request, name="password_reset"),
-    path('reset/<uidb64>/<token>', views.passwordResetConfirm, name='password_reset_confirm'),
+         path("password_change/", views.password_change, name="password_change"),
+    path("password_reset/", views.password_reset_request, name="password_reset"),
+    path('reset/<uidb64>/<token>/', views.passwordResetConfirm, name='password_reset_confirm'),
 ]
