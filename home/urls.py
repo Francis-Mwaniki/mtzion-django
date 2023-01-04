78819b7926d1bff157ab19483508dd547cb0daf0
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-04 12:43:39 +0300

    updated

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.home, name='home'),
-    path("<series>", views.series, name="series"),
-    path("<series>/<topic>", views.topic, name="topic"),
-   path('nav/<username>/',views.nav,name='nav')
+   path("",views.home, name='home'),
+   path("<series>", views.series, name="series"),
+   path("<series>/<topic>", views.topic, name="topic"),
+   path('nav/<username>/',views.nav,name='nav'),
+   path("new_series", views.new_series, name="series-create"),
+   path("new_post", views.new_post, name="post-create"),
+   path("<series>/update", views.series_update, name="series_update"),
+   path("<series>/delete", views.series_delete, name="series_delete"),
+   path("<series>/<topic>/update", views.topic_update, name="topic_update"),
+   path("<series>/<topic>/delete", views.topic_delete, name="topic_delete"),
    ]
