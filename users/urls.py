5a93b3c592a1c12fb0330b87b885ffcc06d7dbdd
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-04 14:47:13 +0300

    updated

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('register/',views.register, name='register'),
     path('login/', views.custom_login, name='login'),
     path('logout/', views.custom_logout, name='logout'),
-    path('profile/<username>/',views.profile,name='profile')
+    path('profile/<username>/',views.profile,name='profile'),
+        path('activate/<uidb64>/<token>', views.activate, name='activate')
 ]
--- users/views.py
+++ users/views.py
@@ -4,11 +4,30 @@ from .forms import UseRegistrationForm, UserLoginForm, UseUpdateForm
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from  .decorators import user_not_authenticated
+from django.template.loader import render_to_string
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes, force_str
+from django.core.mail import EmailMessage
+
+
+from .tokens import account_activation_token
 
 def activateEmail(request, user, to_email):
-    messages.success(request, f'Dear <b>{user}</b>, please go to you email <b>{to_email}</b> inbox and click on \
-        received activation link to confirm and complete the registration. <b>Note:</b> Check your spam folder.')
-
+    mail_subject = 'Activate your user account.'
+    message = render_to_string('template_activate_account.html', {
+        'user': user.username,
+        'domain': get_current_site(request).domain,
+        'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+        'token': account_activation_token.make_token(user),
+        'protocol': 'https' if request.is_secure() else 'http'
+    })
+    email = EmailMessage(mail_subject, message, to=[to_email])
+    if email.send():
+        messages.success(request, f'Dear <b>{user}</b>, please go to you email <b>{to_email}</b> inbox and click on \
+            received activation link to confirm and complete the registration. <b>Note:</b> Check your spam folder.')
+    else:
+        messages.error(request, f'Problem sending confirmation email to {to_email}, check if you typed it correctly.')
 @user_not_authenticated
 def register(request):
     if request.method == "POST":
