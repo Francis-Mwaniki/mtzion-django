74fc1d8005880d83a788947c0e4660eb948c847f
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-04 11:20:10 +0300

    updated

--- base/settings.py
+++ base/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     "crispy_tailwind",
     'captcha',
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 RECAPTCHA_PUBLIC_KEY = '6Lc7ZMsjAAAAAA7v6TEyoHb0hADukccenFhrQ8e_'
 RECAPTCHA_PRIVATE_KEY = '6Lc7ZMsjAAAAAAZkoflOnfcrGtVLmWiKABhPLklg'
