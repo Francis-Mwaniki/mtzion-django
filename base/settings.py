3e3036c07e8a882af56bfb07025c6cdc838bacce
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-04 14:44:17 +0300

    updated

--- base/settings.py
+++ base/settings.py
@@ -164,6 +164,16 @@ USE_I18N = True
 USE_TZ = True
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_FROM = 'francismwaniki630@gmail.com'
+EMAIL_HOST_USER = 'francismwaniki630@gmail.com'
+EMAIL_HOST_PASSWORD = 'rvrwraksqernglgd'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+PASSWORD_RESET_TIMEOUT = 14400
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
