b550e062b4fa617f1c2456f30ffdce3a9d78ec6b
Francis-Mwaniki <francismwaniki630@gmail.com>
2023-01-05 20:15:17 +0300

    updated

--- base/urls.py
+++ base/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
-    path("", include("users.urls")),
+    path("users/", include("users.urls")),
      path('tinymce/', include('tinymce.urls')),
       path("", include("allauth.urls"))
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
